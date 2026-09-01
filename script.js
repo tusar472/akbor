a5de5c9b95c9cd9d0ec4c47e2b27d1ea3ee9244b
tusar472 <mt8856113@gmail.com>
2026-09-02 02:00:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-ializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+ "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, remove, update, set, get } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
